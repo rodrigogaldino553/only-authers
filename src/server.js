cc8d3a449c9ebcf97d1ab8c56cd60713a3ea5c17
rodrigogaldino553 <63233782+rodrigogaldino553@users.noreply.github.com>
2021-04-30 10:44:23 -0300

    login layout done

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ server.use(express.static('public'))
 userRoutes(server)
 require('./app/controllers/ProjectController')(server)
 server.get('/', async(req, res) => {
-    return res.redirect('/landing')
+    return res.redirect('/register')
 })
 
 server.get('/error', async(req, res) => {
