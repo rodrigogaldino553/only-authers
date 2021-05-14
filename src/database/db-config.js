8993b4f9f46cf0034d649598adec1f1efe127374
Rodrigo Galdino <galdinorodrigo553@gmail.com>
2021-05-14 13:57:27 -0300

    Update db-config.js

--- src/database/db-config.js
+++ src/database/db-config.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise')
 require('dotenv/config')
 
-const database = {host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE}
+const database = {host: 'remotemysql.com',//process.env.MYSQL_HOST,
+        user: 'HCzqH6c6AM', //process.env.MYSQL_USER,
+        password: 'agKX8a5sa5',//process.env.MYSQL_PASSWORD,
+        database: 'HCzqH6c6AM'//process.env.MYSQL_DATABASE}
 
 async function connect(){
 
