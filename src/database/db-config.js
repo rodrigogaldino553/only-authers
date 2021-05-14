6c337d2b758f9f723ff28b9e6fd533a0978886e5
Rodrigo Galdino <galdinorodrigo553@gmail.com>
2021-05-14 14:01:59 -0300

    Update db-config.js

--- src/database/db-config.js
+++ src/database/db-config.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql2/promise')
 require('dotenv/config')
 
-const database = {host: 'remotemysql.com',//process.env.MYSQL_HOST,
-        user: 'HCzqH6c6AM', //process.env.MYSQL_USER,
+
+const host = process.env.MYSQL_HOST
+const user = process.env.MYSQL_USER
+
+const database = {host: host,//process.env.MYSQL_HOST,
+        user: user, //process.env.MYSQL_USER,
         password: 'agKX8a5sa5',//process.env.MYSQL_PASSWORD,
         database: 'HCzqH6c6AM'}//process.env.MYSQL_DATABASE}
-console.log(process.env.MYSQL_HOST)
+console.log(process.env.MYSQL_HOST, user)
 async function connect(){
 
     if(global.connection && global.connection.state !== 'disconnected') return global.connection
