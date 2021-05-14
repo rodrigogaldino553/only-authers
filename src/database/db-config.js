c7cf5a517d7b3f9c88c7fb2bbc84effdc47d9f17
Rodrigo Galdino <galdinorodrigo553@gmail.com>
2021-05-14 13:58:02 -0300

    Update db-config.js

--- src/database/db-config.js
+++ src/database/db-config.js
@@ -5,7 +5,7 @@ const database = {host: 'remotemysql.com',//process.env.MYSQL_HOST,
         user: 'HCzqH6c6AM', //process.env.MYSQL_USER,
         password: 'agKX8a5sa5',//process.env.MYSQL_PASSWORD,
         database: 'HCzqH6c6AM'//process.env.MYSQL_DATABASE}
-
+console.log(process.env.MYSQL_HOST)
 async function connect(){
 
     if(global.connection && global.connection.state !== 'disconnected') return global.connection
